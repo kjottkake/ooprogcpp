2499ab08f6bbf7299772be70fc6c880cf40abedd
Jason <yunj@stud.ntnu.no>
2021-03-18 14:03:48 +0100

    first commit all files

---
+++ cmake-build-debug/CMakeFiles/oblig3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/oblig3.dir/main.cpp.o"
+  "oblig3"
+  "oblig3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/oblig3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
